69997e8addd068ee3e25fb701788e822f3bcd049
sreeraje <sreerajii96@gmail.com>
2016-06-30 15:39:03 +0530

    Create collfl.c

---
+++ collfl.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+ int a,b;
+  printf("Enter the maximum number of times\n");
+  scanf("%d",&a);
+printf("Enter the number of games played\n");
+  scanf("%d",&b);
+  if(a>b)
+  {
+    printf("IN\n");
+  }
+  else 
+  {
+    printf("OUT\n");
+  }
+  return 0;
+}
