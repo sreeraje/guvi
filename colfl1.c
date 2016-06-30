cc036125da2d2c21916c2748dc0d7aab447beb43
sreeraje <sreerajii96@gmail.com>
2016-06-30 15:39:43 +0530

    Create colfl1.c

---
+++ colfl1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int y;
+  scanf("%d",&y);
+  if(y%2==0)
+  {
+    printf("%d is an even number",y);
+  }
+  else
+  {
+    printf("%d is an odd number",y);
+  }
+  return 0;
+}
