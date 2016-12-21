982d656aa7c4b64c8eaf9f7f7fae7d262d1e1dfe
sreeraje <sreerajii96@gmail.com>
2016-12-21 15:28:41 +0530

    Create three1.c

---
+++ three1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int n,a=100,b=50,c=45;
+printf("enter the integer");
+scanf("%d",&n);
+if(a<=b)
+{
+printf("a is big");
+}
+else if(b<=c)
+{
+printf("b is big");
+}
+else
+{
+printf("c is big");
+}
+}
