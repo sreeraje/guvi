028e741a5d357ee072641c94317f7de29f3a28a6
sreeraje <sreerajii96@gmail.com>
2016-12-23 15:28:14 +0530

    Create multiply2.c

---
+++ multiply2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int n,i=1;
+printf("enter the number");
+scanf("%d",&n);
+printf("multiplication of a table %d",n);
+while(i<=10)
+{
+printf("%d * %d =%d",n,i,n*i;
+i++;
+}
+return 0;
+}
