7cc918928805ce592731eab24aa695d7de6fe891
sreeraje <sreerajii96@gmail.com>
2017-04-21 11:03:15 +0530

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,14 @@
+import java.io.*;
+class factorial
+{
+public static void main(String args[])
+{
+int i,fact=1;
+int n=5;
+for(i=0;i<=n;i++)
+{
+fact=fact*i;
+}
+System.out.println("factorial of a number is",fact);
+}
+}
