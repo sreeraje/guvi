800305ab03f78e727920c8cba8e1abd3a4e20d2c
sreeraje <sreerajii96@gmail.com>
2017-06-29 11:38:51 +0530

    Create tri.c

---
+++ tri.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,c,k;
+clrscr();
+scanf("%d",&n);
+for(c=1;c<=n;c++)
+{
+for(k=1;k<=c;k++)
+printf("*");
+printf("\n");
+}
+getch();
+return 0;
+}
